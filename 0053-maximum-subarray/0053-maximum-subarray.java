f59fd9270b085e168ac51d2e81e37b27c1fb5989
Sivareddy Uppathi <sivareddy.uppathi@gmail.com>
2022-10-26 16:59:03 +0530

    Time: 2 ms (78.86%), Space: 73.5 MB (55.70%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,12 +1,35 @@
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+        
+//         int curMax = nums[0], max = nums[0];
+        
+//         for(int i=1; i< nums.length; i++) {
+//             curMax = Math.max((curMax + nums[i]), nums[i]);
+//             max = Math.max(curMax, max);
+//         }
+//         return max;
+//     }
+// }
+
 class Solution {
     public int maxSubArray(int[] nums) {
+        // need to fill in the dp table 
         
-        int curMax = nums[0], max = nums[0];
-        
-        for(int i=1; i< nums.length; i++) {
-            curMax = Math.max((curMax + nums[i]), nums[i]);
-            max = Math.max(curMax, max);
+        // we can just fill in the dp table by itself since we are only returning our solution and not the list itself that contains the integers
+        // either include the ith element or you don't
+        // Opt(i) = 
+        if(nums.length == 1) {
+            return nums[0];
+        }
+        int max = nums[0]; // this is our default value
+        // we are doing bottom up dynamic programming
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i-1] + nums[i], nums[i]);
+            if(nums[i] > max) {
+                max = nums[i];
+            }
         }
         return max;
+        
     }
 }
